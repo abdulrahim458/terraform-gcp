01c9741346c98f59642750fecc9689947dfb3941
abdulrahim458 <67860725+abdulrahim458@users.noreply.github.com>
2022-10-10 01:08:08 +0530

    updating roles

--- terraform/svc.tf
+++ terraform/svc.tf
@@ -29,6 +29,30 @@ resource "google_service_account_key" "mykey" {
   public_key_type    = "TYPE_X509_PEM_FILE"
 }
 
+
+resource "google_service_account_iam_member" "terraform-account-iam" {
+    for_each = toset([
+    "roles/cloudbuild.admin",
+    "roles/compute.Admin",
+    "roles/compute.instanceAdmin",
+    "roles/compute.networkAdmin",
+    "roles/storage.admin",
+    "roles/compute.loadBalancerAdmin",
+    "roles/iam.service.AccountUser",
+  ])
+  service_account_id = google_service_account.abdul.name
+  role               = each.key
+  member = "serviceAccount:${google_service_account.sa.email}"
+}
+
+
+
+resource "google_service_account_iam_member" "webserver-start-iam" {
+  service_account_id = google_service_account.abdul.name
+  role               = "roles/compute.instances.start"
+  member = "user:abdulrahim458@gmail.com"
+}
+
 output "gcp_bucket" {
   value = google_storage_bucket.tf-bucket
 }
